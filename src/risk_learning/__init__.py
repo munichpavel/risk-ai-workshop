2d4bbed5c1919cefe10a669688f2daf6559cb267
munichpavel <munichpavel@gmail.com>
2019-06-03 20:56:55 +0200

    Initial commit

---
+++ src/risk_learning/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for risk-learning."""
+
+__author__ = """Paul Larsen"""
+__email__ = 'munichpavel@gmail.com'
+__version__ = '0.1.0'
---
+++ tests/test_risk_learning.py
@@ -0,0 +1,6 @@
+"""Tests for `risk_learning` package."""
+
+import pytest
+
+
+from risk_learning import risk_learning
