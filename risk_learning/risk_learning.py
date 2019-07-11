5ddb8eace209a4737f44f50c46bf861f4b7826dd
munichpavel <munichpavel@gmail.com>
2019-07-11 20:27:24 +0200

    Add get family name

--- risk_learning/risk_learning.py
+++ risk_learning/risk_learning.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+def get_classifier_family_name(clf):
+    res = str(clf)
+    res = res.split('(')[0]
+    return res
+    #return 'LogisticRegression'
--- tests/test_risk_learning.py
+++ tests/test_risk_learning.py
@@ -1,3 +1,9 @@
 """Tests for `risk_learning` package."""
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
 
-import pytest
+from risk_learning.risk_learning import get_classifier_family_name
+
+def test_get_classifier_family_name():
+    assert get_classifier_family_name(LogisticRegression()) == 'LogisticRegression'
+    assert get_classifier_family_name(GradientBoostingClassifier()) == 'GradientBoostingClassifier'
