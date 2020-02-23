71741bd5a6ce4fda860972f88fcf85df8387d3f8
munichpavel <munichpavel@gmail.com>
2020-02-23 14:27:53 +0100

    Remove deprecated tests

--- tests/test_risk_learning.py
+++
@@ -1,9 +0,0 @@
-"""Tests for `risk_learning` package."""
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import GradientBoostingClassifier
-
-from risk_learning.risk_learning import get_classifier_family_name
-
-def test_get_classifier_family_name():
-    assert get_classifier_family_name(LogisticRegression()) == 'LogisticRegression'
-    assert get_classifier_family_name(GradientBoostingClassifier()) == 'GradientBoostingClassifier'
