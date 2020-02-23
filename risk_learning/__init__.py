c27a5cd433a866255fdf67cae7d77c1208fd1625
munichpavel <munichpavel@gmail.com>
2020-02-23 08:01:06 +0100

    Remove deprecated code

--- risk_learning/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for risk-learning."""
-
-__author__ = """Paul Larsen"""
-__email__ = 'munichpavel@gmail.com'
-__version__ = '0.1.0'
--- risk_learning/risk_learning.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Main module."""
-def get_classifier_family_name(clf):
-    res = str(clf)
-    res = res.split('(')[0]
-    return res
-    #return 'LogisticRegression'
