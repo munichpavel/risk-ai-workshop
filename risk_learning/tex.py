33d383197c1487bf5256c350436b6290a9321a91
munichpavel <munichpavel@gmail.com>
2022-03-15 10:45:44 +0100

    Run bibtex on slides for references

--- risk_learning/tex.py
+++ risk_learning/tex.py
@@ -2,12 +2,40 @@
 
 from pathlib import Path
 import subprocess
+import shutil
 
 
 def build_pdf_latex(filepath: Path, outdir: Path):
+    '''
+    Perform combination of pdf latex and bibtex, using the recipe taught to me
+    by https://www.sfu.ca/~nilten/ way back when
+    '''
+
+    n_reps_pdf_latex = 2
+
+    for _ in range(n_reps_pdf_latex):
+        subprocess.run(
+            ['pdflatex', filepath],
+            cwd=filepath.parent.as_posix()
+        )
+
+    # bibtex
     subprocess.run(
-        ['pdflatex', f'-output-directory={outdir}', filepath],
+        ['bibtex', filepath],
         cwd=filepath.parent.as_posix()
+    )
+
+    # pdflatex again, this time use output directory
+    for _ in range(n_reps_pdf_latex):
+        subprocess.run(
+            ['pdflatex', filepath],
+            cwd=filepath.parent.as_posix()
+        )
+
+    pdf_out_filename = filepath.stem + '.pdf'
+    shutil.copy(
+        src=filepath.parent / pdf_out_filename,
+        dst=outdir / pdf_out_filename
     )
 
 
