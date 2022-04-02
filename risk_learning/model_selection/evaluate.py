e24eb034f083cc3a057801707d35c92dcb819297
munichpavel <munichpavel@gmail.com>
2022-04-02 08:12:07 +0200

    Add mean score per gender to evaluate stage
    and put metrics under version control so I can use
    
    dvc metrics diff <metric-file-path>

--- risk_learning/model_selection/evaluate.py
+++ risk_learning/model_selection/evaluate.py
@@ -7,8 +7,15 @@ import json
 import pandas as pd
 import sklearn.metrics as metrics
 
+from risk_learning.simpson import (
+    get_flat_combinations, make_feature_combination_score_array
+)
 
-def main(stage_name: str, stage_params: dict) -> None:
+
+def main(
+    stage_name: str, stage_params: dict, domain_value_params: dict,
+    target_col: str
+) -> None:
     project_root = Path(os.environ['PROJECT_ROOT'])
     data_dir = project_root / 'notebooks' / 'data'
     input_dir = data_dir / stage_params['data_in_folder']
@@ -35,16 +42,44 @@ def main(stage_name: str, stage_params: dict) -> None:
         )
 
     # Evaluate models
+    feature_combination_dict = {}
+    for feature_name, domain_values in domain_value_params.items():
+        print(feature_name, target_col)
+        if feature_name == target_col:
+            print('Target column, not a feature. Skipping.')
+            continue
+        feature_combination_dict[feature_name] = domain_values
+
+    feature_combinations = get_flat_combinations(feature_combination_dict)
+
     for model_path in model_paths:
         model_name = model_path.stem
         with open(outdir / f'metrics_{model_name}.json', "w") as fp:
             clf = joblib.load(model_path)
+
+            # Evaluate on the dataset
             predictions_by_class = clf.predict_proba(X)
-            predictions = predictions_by_class[:, 1]
-            avg_prec = metrics.average_precision_score(y, predictions)
+            scores = predictions_by_class[:, 1]
+            avg_prec = metrics.average_precision_score(y, scores)
+
+            # Evaluate on the feature combinations
+            feature_scores = clf.predict_proba(feature_combinations)[:, 1]
+            feature_score_array = make_feature_combination_score_array(
+                feature_combinations, feature_scores
+            )
+            mean_female_score = float(
+                feature_score_array.sel(gender=0).mean().values
+            )
+            mean_male_score = float(
+                feature_score_array.sel(gender=1).mean().values
+            )
 
             json.dump(
-                {"avg_prec": avg_prec},
+                {
+                    'avg_precision': avg_prec,
+                    'mean_female_score': mean_female_score,
+                    'mean_male_score': mean_male_score
+                },
                 fp, indent=4
             )
 
@@ -63,5 +98,8 @@ if __name__ == '__main__':
 
     params = get_params()
     stage_params = params[args.stage_name]
+    domain_value_params = params['data_models']['domain_values']
+    split_params = params['split']
+    target_col = split_params['target_col']
 
-    main(args.stage_name, stage_params)
+    main(args.stage_name, stage_params, domain_value_params, target_col)
