5c5df31fffeaef71f8636fb57c29e277aa344003
Paul Larsen <munichpavel@gmail.com>
2024-05-22 20:31:36 +0200

    Bump version: 2024.0.0 → 2024.1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='risk_learning',
-    version='2024.0.0',
+    version='2024.1.0',
     author="Paul Larsen",
     author_email='munichpavel@gmail.com',
     url='https://github.com/munichpavel/risk_learning',
